96c234bdc4961905012a17f62fe5aa3ee20c3e17
Rzkdriech <ucihasaskee07@gmail.com>
2024-02-28 12:55:02 +0800

    initial routes

--- index.js
+++ index.js
@@ -9,4 +9,17 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// routes management
+import userRoute from "./src/routes/User.js"
+
+const routes = [
+    userRoute,
+  ];
+
+routes.forEach((route) => {
+app.use(route);
+});
+  
+
 app.listen(PORT, () => console.log(`Server running in ${PORT}`));
