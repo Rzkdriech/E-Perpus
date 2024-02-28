113b1c054301fd31d307eb6bb127f15d6bff364b
akhdrzki <akhadirizky@gmail.com>
2024-02-28 20:03:08 +0800

    update: category (to create some categories)

--- src/controllers/Kategori.js
+++ src/controllers/Kategori.js
@@ -9,3 +9,26 @@ export const getKategori = async (_, res) => {
   }
 };
 
+export const createKategori = async (req, res) => {
+  const newKategori = req.body;
+
+  const dataExist = await prisma.kategoribuku.findMany({
+    where: {
+      NamaKategori: newKategori.NamaKategori,
+    },
+  });
+
+  if (dataExist.length > 0) return res.status(400).json({ msg: "data exists" });
+
+  try {
+    const kategori = await prisma.kategoribuku.create({
+      data: {
+        NamaKategori: newKategori.NamaKategori
+      },
+    });
+    res.status(201).json({ msg: "created data", data: kategori });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
