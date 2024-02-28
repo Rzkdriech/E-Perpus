15d20ae594f060828b8da11fc09d47ea61e0e18e
Rzkdriech <ucihasaskee07@gmail.com>
2024-02-28 13:17:49 +0800

    update: put register/create an account controllers

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,10 +1,43 @@
 import { prisma } from "../database/db.js";
 
 export const getUsers = async (_, res) => {
-    try {
-      const response = await prisma.user.findMany();
-      res.status(200).json(response);
-    } catch (error) {
-      res.status(500).json({ msg: error.message });
+  try {
+    const response = await prisma.user.findMany();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const createUser = async (req, res) => {
+  const { Password, ConfPass } = req.body;
+  const newUser = req.body;
+
+  // Password
+  if (Password != ConfPass)
+    return res.status(400).json({ msg: "Pastikan password anda benar" });
+
+  const checkUser = await prisma.user.findMany({
+    where: {
+      Email: newUser.Email
     }
-  };
+  });
+
+  if (checkUser.length > 0) return res.status(400).json({ msg: "User exist" });
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        Username: newUser.Username,
+        Password: Password,
+        Email: newUser.Email,
+        Nama_lengkap: newUser.Nama_lengkap,
+        Alamat: newUser.Alamat,
+        Role: newUser.Role
+      }
+    });
+    res.status(201).json({ msg: "data created", data: user });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
