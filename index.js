8678aa6073ddc31f4a669b2a1af75768c8ceb481
akhdrzki <akhadirizky@gmail.com>
2024-02-28 19:44:49 +0800

    change minor: remove "/" route

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", (_, res) => {
-  res.send("halo gays")
-})
+
 
 // routes management
 import userRoute from "./src/routes/User.js";
