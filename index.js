fdb4ffa87272cefe2484880385b871e45a5866f2
akhdrzki <akhadirizky@gmail.com>
2024-02-28 19:19:05 +0800

    update: books route on index

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", (_, res) => {
+  res.send("halo gays")
+})
+
 // routes management
 import userRoute from "./src/routes/User.js";
-
-const routes = [userRoute];
+import bukuRoute from "./src/routes/Buku.js";
+const routes = [userRoute, bukuRoute];
 
 routes.forEach((route) => {
   app.use(route);
