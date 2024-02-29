0f07744fac14c6b38df35a24f3f3413bac9f7e04
Rzkdriech <ucihasaskee07@gmail.com>
2024-02-29 10:35:38 +0800

    add: auth to secure all route

---
+++ src/middleware/Auth.js
@@ -0,0 +1,26 @@
+import { prisma } from "../database/db.js";
+
+export const verifyUser = async (req, res, next) => {
+  try {
+    console.log(req.session);
+    if (!req.session.username) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    const users = await prisma.user.findMany({
+      where: {
+        Username: req.session.username
+      }
+    });
+
+    if (users.length === 1) {
+      req.user = users[0];
+      next();
+    } else {
+      return res.status(401).send("Unauthorized");
+    }
+  } catch (error) {
+    console.error("Error checking authentication:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
