927242fd98a6361b61998b221b7724adbaebde8f
Rzkdriech <ucihasaskee07@gmail.com>
2024-02-29 10:34:21 +0800

    update: initial authRoute

--- index.js
+++ index.js
@@ -9,12 +9,34 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
+
+// session
+import session from "express-session";
+import { MemoryStore } from "express-session";
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 900000,
+      secure: false,
+      sameSite: true
+    },
+    secret: "halogays",
+    resave: false,
+    saveUninitialized: true,
+    store: new MemoryStore()
+  })
+);
+
 // routes management
 import userRoute from "./src/routes/User.js";
 import bukuRoute from "./src/routes/Buku.js";
 import kategoriRoute from "./src/routes/Kategori.js";
-import katRelasi from "./src/routes/KategoriRelasi.js"
-const routes = [userRoute, bukuRoute, kategoriRoute, katRelasi];
+import katRelasi from "./src/routes/KategoriRelasi.js";
+import authRoute from "./src/routes/Auth.js";
+
+const routes = [userRoute, bukuRoute, kategoriRoute, katRelasi, authRoute];
 
 routes.forEach((route) => {
   app.use(route);
